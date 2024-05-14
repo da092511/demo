7b1dce3f6143d78ce3342434b6dad5ab1258b07c
da092511 <da092511@naver.com>
2024-05-14 18:34:05 +0900

    Modify system color mode

--- src/theme.js
+++ src/theme.js
@@ -6,10 +6,10 @@ import { extendTheme } from '@chakra-ui/react'
 // 2. Add your color mode config
 const config = {
     initialColorMode: 'light',
-    useSystemColorMode: true
+    useSystemColorMode: false
 }
 
 // 3. extend the theme
 const theme = extendTheme({ config })
 
-export default theme
+export default theme;
